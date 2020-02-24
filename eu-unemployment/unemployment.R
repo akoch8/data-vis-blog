35c61388a7f25b121fc1ceed1c2ad4862f0b1b48
Alexander Koch <alexander.koch@ugent.be>
2020-02-24 16:37:33 +0100

    Added some explanatory comments

--- eu-unemployment/unemployment.R
+++ eu-unemployment/unemployment.R
@@ -1,9 +1,17 @@
+###
+### Visualizing the EU unemployment rates by NUTS2 region
+###
+### Alexander Koch
+### 2018
+###
+
 library(eurostat)
 library(rworldmap)
 library(rworldxtra)
 
 # Load and explore the Eurostat unemployment rate data.
 data = data.frame(get_eurostat('tgs00010'), stringsAsFactors=F)
+
 # Select the most recent data for both men and women.
 data = data[data$time == '2017-01-01' & data$sex == 'T',]
 dim(data)
@@ -21,6 +29,7 @@ valueCol = vector()
 naCol = '#fffffe'
 for (i in 1:nrow(mapData)) {
 	nutsID = as.character(mapData$NUTS_ID[i])
+	
 	# Check if the region (NUTS) ID is used in the unemployment data. If it is, we can
 	# map it to a color. If it isn't we'll assign that region the "missing value" color.
 	if (nutsID %in% data$geo) {
@@ -43,6 +52,7 @@ plot(mapData, col=valueCol, border=NA, bg='#fffffe', xlim=c(-1800000, 3900000), 
 worldMap = getMap(resolution='high')
 worldMap = spTransform(worldMap, CRS('+proj=wintri'))
 plot(worldMap, add=T, border='#2c2c2c', lwd=0.25)
+
 # Add a legend to the plot. The x and y values are the result of (a lot of) trial and
 # error.
 rect(-550000, 3050000, 2750000, 3700000, col='#fffffe', border='#2c2c2c', lwd=0.25)
